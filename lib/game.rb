cd1cf55c6d303f94e85cd47d61ccd3b623e9a0eb
JCembala <cembala.jakub@gmail.com>
2022-05-23 12:21:49 +0200

    add accessors

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,8 @@
 
 # Holds information about game
 class Game
+  attr_accessor :name, :priority, :max_player_count
+
   def initialize(name, priority = 1, max_player_count = 2)
     @name = name
     @priority = priority
--- lib/player.rb
+++ lib/player.rb
@@ -4,13 +4,11 @@ require 'date'
 
 # Holds information about player
 class Player
+  attr_accessor :games, :name
+
   def initialize(name, games = [])
     @name = name
     @games = games
     @sign_up_at = DateTime.now
-  end
-
-  def to_s
-    "Player name: #{@name}\n#{@sign_up_at.strftime("created at: %m/%d/%Y %I:%M%p")}\n#{@games.to_s}"
   end
 end
