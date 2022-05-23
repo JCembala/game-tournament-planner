c23a5d307ec3c3dcf8ba357874d02a60a161d123
JCembala <cembala.jakub@gmail.com>
2022-05-23 08:59:53 +0200

    add test entries

--- lib/main.rb
+++ lib/main.rb
@@ -4,12 +4,26 @@ require_relative 'tournament_planner'
 
 games = [
   Game.new('Cards 1', 5, 4),
-  Game.new('Cards 2', 4, 2)
+  Game.new('Cards 2', 4, 2),
+  Game.new('Cards 3', 4, 2),
+  Game.new('Cards 4', 4, 2),
+  Game.new('Cards 5', 4, 3),
+  Game.new('Cards 6', 4, 3),
+  Game.new('Cards 7', 5, 3),
+  Game.new('Cards 8', 6, 2),
+  Game.new('Cards 9', 6, 2),
+  Game.new('Cards 10', 10, 2)
 ]
 
 players = [
-  Player.new('Adam', [games[0], games[1]]),
-  Player.new('Jan', [games[0]])
+  Player.new('Adam A', [games[0], games[6], games[7]]),
+  Player.new('Adam B', [games[0], games[6]]),
+  Player.new('Adam C', [games[0], games[6]]),
+  Player.new('Adam D', [games[0], games[4]]),
+  Player.new('Adam E', [games[1], games[4]]),
+  Player.new('Adam F', [games[1], games[3]]),
+  Player.new('Adam G', [games[1], games[3]]),
+  Player.new('Adam H', [games[4]])
 ]
 
 tournament = TournamentPlanner.new(players, games)
