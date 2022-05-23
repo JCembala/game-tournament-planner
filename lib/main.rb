3f6f672ad0b3730dc6b75dea872af8503ba32c49
JCembala <cembala.jakub@gmail.com>
2022-05-23 12:24:07 +0200

    modify initial data

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: false
 
+# There is a board games day scheduled and organizers would like to automatically
+# get a list of all game tournaments & players that will take place during that day
+# according to the preference of the attendees.
+
 require_relative 'tournament_planner'
 
 games = [
   Game.new('Cards 1', 5, 4),
-  Game.new('Cards 2', 4, 2),
+  Game.new('Cards 2', 10, 2),
   Game.new('Cards 3', 4, 2),
   Game.new('Cards 4', 4, 2),
   Game.new('Cards 5', 4, 3),
@@ -16,11 +20,11 @@ games = [
 ]
 
 players = [
-  Player.new('Adam A', [games[0], games[6], games[7]]),
-  Player.new('Adam B', [games[0], games[6]]),
+  Player.new('Adam A', [games[5], games[6], games[7]]),
+  Player.new('Adam B', [games[5], games[7]]),
   Player.new('Adam C', [games[0], games[6]]),
-  Player.new('Adam D', [games[0], games[4]]),
-  Player.new('Adam E', [games[1], games[4]]),
+  Player.new('Adam D', [games[7], games[4]]),
+  Player.new('Adam E', [games[6], games[4]]),
   Player.new('Adam F', [games[1], games[3]]),
   Player.new('Adam G', [games[1], games[3]]),
   Player.new('Adam H', [games[4]])
