0fad34f6a6aa3c423bf16fd896bd7c83ae6e00ca
JCembala <cembala.jakub@gmail.com>
2022-05-24 11:36:16 +0200

    add randomizing games and players

--- lib/main.rb
+++ lib/main.rb
@@ -6,29 +6,11 @@
 
 require_relative 'tournament_planner'
 
-games = [
-  Game.new('Cards 1', 5, 4),
-  Game.new('Cards 2', 10, 2),
-  Game.new('Cards 3', 4, 2),
-  Game.new('Cards 4', 4, 2),
-  Game.new('Cards 5', 4, 3),
-  Game.new('Cards 6', 4, 3),
-  Game.new('Cards 7', 5, 3),
-  Game.new('Cards 8', 6, 2),
-  Game.new('Cards 9', 6, 2),
-  Game.new('Cards 10', 10, 2)
-]
+games = []
+10.times { |i| games << Game.new("Game #{i}", rand(1..11), rand(2..5)) }
 
-players = [
-  Player.new('Adam A', [games[5], games[6], games[7]]),
-  Player.new('Adam B', [games[5], games[7]]),
-  Player.new('Adam C', [games[0], games[6]]),
-  Player.new('Adam D', [games[7], games[4]]),
-  Player.new('Adam E', [games[6], games[4]]),
-  Player.new('Adam F', [games[1], games[3]]),
-  Player.new('Adam G', [games[1], games[3]]),
-  Player.new('Adam H', [games[4]])
-]
+players = []
+30.times { |i| players << Player.new("Player #{i}", games.sample(rand(3..11))) }
 
 tournament = TournamentPlanner.new(players, games)
 
